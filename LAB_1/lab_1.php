abc06457f592ba1eaeda70ff853bad8aa3c68edc
Shaon Basak <shaonbasaksb@gmail.com>
2025-11-03 18:52:52 +0600

    Created Regestration Page

---
+++ LAB_1/lab_1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Registration Information</title>
+</head>
+<body>
+<h1 align="left" style="color: red; border-bottom: 3px solid red;">Student Registration Information</h1>
+<br>
+ First Name:<br>
+<input type ="text "> <br>
+ Last Name:<br>
+<input type ="text "> <br>
+Student ID :<br>
+<input type="Number"> <br>
+Program/Major:<br>
+<input type="Dropbox"> <br>
+Department:<br>
+<input type="Dropbox"> <br>
+ Phone:<br>
+<input type ="text "> <br>
+ Univesity Email:<br>
+<input type ="text "> <br>
+ Create Password(min 8 characters):<br>
+<input type ="text "> <br>
+ Confirm Password:<br>
+<input type ="text "> <br>
+Semester Selection :<br>
+<input type="radio">Summer 2024
+<input type="radio">Fall 2024
+<input type="radio">Spring 2024
+<input type="radio">Other/Special Term<br>
+Requested Credit Load(Max 15):<br>
+<input type ="text "> <br>
+ <input type="Checkbox">I require academic advising before final registration.
+<br><br>
+<h1 align="left" style="color: red; border-bottom: 3px solid red;">Course Selection</h1><br>
+ 
+<input type="Checkbox">MATH 1201 (Calculus)
+<input type="Checkbox">CS 2105(Data Structures)
+<input type="Checkbox">ECON 1001(Microeconomics)<br>
+<input type="Checkbox">Physics Lab)
+<br><br>
+ Comments/Special Requests:<br>
+<input type ="text "> <br>
+
+<center><input type="Submit"> </center> <br> 
+<center><input type="reset"> </center>
+
+
+
+</body>
+</html>
